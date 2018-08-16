f358a9ea6b8159bc68a531121e58a3204bb04b22
scorp200 <abca2004@gmail.com>
2018-08-15 21:48:16 -0400

    first steps
    made planets that can orbit planets, that can orbit planets, that can orbit planets, that can orbit planets...

--- game.js
+++ game.js
@@ -1,34 +1,56 @@
 // Cache stuff.
 var addEventListener = document.addEventListener;
 var Canvas = document.getElementById("c");
-
+var ctx = Canvas.getContext("2d");
+var log = console.log;
+var cr = 2 * Math.PI;
+var w = 1200,
+	h = 800;
+Canvas.width = w;
+Canvas.height = h;
+ctx.fillStyle = '#4d4d4d';
+ctx.fillRect(0, 0, 1200, 1200);
+ctx.clearRect(0, 0, 1200, 800);
 // Disables right click context menu.
 addEventListener("contextmenu", function(e) {
 	e.preventDefault();
 });
-
+var p = new Planet('#ffe200', 40, w / 2, h / 2);
+var p2 = new Planet('#77c3c3', 10, 0, 0);
+var p3 = new Planet('#666666', 5, 0, 0);
+p.addOrbit(p2, 200, 0.005);
+p2.addOrbit(p3, 50, -0.01);
 // Create Mouse object.
 // IIFE. Sets up events and returns basic object.
 var Mouse = (function() {
 
-    // Basic object setup.
-    var object = { x: 0, y: 0 };
+	// Basic object setup.
+	var object = { x: 0, y: 0 };
 
-    // Update mouse position.
-    // Can probably remove "rect" stuff if canvas is whole window.
-    addEventListener("mousemove", function(e) {
-        var rect = Canvas.getBoundingClientRect();
-        object.x = e.clientX - rect.left;
-        object.y = e.clientY - rect.top;
-    });
+	// Update mouse position.
+	// Can probably remove "rect" stuff if canvas is whole window.
+	addEventListener("mousemove", function(e) {
+		var rect = Canvas.getBoundingClientRect();
+		object.x = e.clientX - rect.left;
+		object.y = e.clientY - rect.top;
+	});
 
-    //
-    addEventListener("mousedown", function(e) {
-        // ADD AN "ONCLICK" FUNCTION HERE.
-        e.preventDefault();
-    });
+	//
+	addEventListener("mousedown", function(e) {
+		// ADD AN "ONCLICK" FUNCTION HERE.
+		e.preventDefault();
+	});
 
-    // Return our object.
-    return object;
+	// Return our object.
+	return object;
 
 })();
+update();
+
+function update() {
+	requestAnimationFrame(update);
+	ctx.clearRect(0, 0, w, h)
+	p.update();
+	p2.update();
+	p3.update();
+}
---
+++ planet.js
@@ -0,0 +1,21 @@
+var Planet = function(color, size, x, y) {
+	var t = this;
+	t.orbits = [];
+	t.x = x;
+	t.y = y;
+	t.update = function() {
+        ctx.beginPath();
+		ctx.fillStyle = color;
+		ctx.arc(t.x, t.y, size, 0, cr);
+        ctx.closePath();
+		ctx.fill();
+		t.orbits.forEach(function(e) {
+			e.angle += e.speed;
+			e.planet.x = e.distance * Math.cos(e.angle) + t.x;
+			e.planet.y = e.distance * Math.sin(e.angle) + t.y;
+		});
+	};
+	t.addOrbit = function(planet, distance, speed, angle = 0) {
+		t.orbits.push({ planet: planet, distance: distance, speed: speed, angle: angle });
+	}
+}
