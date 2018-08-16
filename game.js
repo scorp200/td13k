50beddc0f631fc1f80d64d1718d6b643bf30305d
JackOatley <jack.f.oatley@gmail.com>
2018-08-16 12:13:08 +0100

    Changed game loop to have separate update and render functions.

--- game.js
+++ game.js
@@ -1,26 +1,33 @@
 // Cache stuff.
-var addEventListener = document.addEventListener,
-	Canvas = document.getElementById("c"),
-	ctx = Canvas.getContext("2d"),
-	log = console.log,
-	cr = 2 * Math.PI,
-	w = 1200,
-	h = 800,
-	planets = [];
+var doc = document;
+var addEventListener = doc.addEventListener;
+var Canvas = doc.getElementById("c");
+var ctx = Canvas.getContext("2d");
+var log = console.log;
+var cr = 2 * Math.PI;
+var w = 1200;
+var h = 800;
+var planets = [];
+
+// Disables right click context menu.
+addEventListener("contextmenu", function(e) {
+	e.preventDefault();
+});
+
+// Setup Canvas (move to graphics.js)
 Canvas.width = w;
 Canvas.height = h;
 ctx.fillStyle = '#4d4d4d';
 ctx.fillRect(0, 0, 1200, 1200);
 ctx.clearRect(0, 0, 1200, 800);
-// Disables right click context menu.
-addEventListener("contextmenu", function(e) {
-	e.preventDefault();
-});
+
+// Setup planets (procgen this?)
 planets.push(new Planet('#ffe200', 40, w / 2, h / 2));
 planets.push(new Planet('#77c3c3', 10, 0, 0));
 planets.push(new Planet('#666666', 5, 0, 0));
 planets[0].addOrbit(planets[1], 200, 0.005);
 planets[1].addOrbit(planets[2], 50, -0.01);
+
 // Create Mouse object.
 // IIFE. Sets up events and returns basic object.
 var Mouse = (function() {
@@ -48,8 +55,32 @@ var Mouse = (function() {
 })();
 
 // Update game.
-(function update() {
-	requestAnimationFrame(update);
-	ctx.clearRect(0, 0, w, h)
-	planets.forEach(function(e) { e.update(); })
+var lastTick = performance.now();
+var tickLength = 1000 / 60; // Logic steps per second
+var last = 0;
+(function frame(timestamp) {
+
+	requestAnimationFrame(frame);
+	var nextTick = lastTick + tickLength;
+	var numTicks = 0;
+
+	if (timestamp > nextTick) {
+		let timeSinceTick = timestamp - lastTick;
+		var numTicks = Math.floor(timeSinceTick / tickLength);
+		for (var n=0; n<Math.min(numTicks, 60); n++) {
+			update();
+		}
+	}
+
+	render();
+
 })();
+
+function update() {
+	//planets.forEach(function(e) { e.update(); })
+}
+
+function render() {
+	ctx.clearRect(0, 0, w, h);
+	planets.forEach(function(e) { e.update(); })
+}
