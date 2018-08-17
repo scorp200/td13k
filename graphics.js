a112dfe18eb09d7b83719491464db777370fa122
JackOatley <jack.f.oatley@gmail.com>
2018-08-17 15:22:03 +0100

    Working on shadows.

--- graphics.js
+++ graphics.js
@@ -15,20 +15,49 @@ addEventListener("resize", resize);
 
 // Render orbit
 function renderOrbit(body) {
-    if (body.orbit) {
+    var orbit = body.orbit;
+    if (orbit) {
         ctx.beginPath();
+        ctx.strokeStyle = body.color + "33";
+        ctx.setLineDash([5,5])
+        ctx.shadowBlur = 2;
+        ctx.shadowColor = body.color;
+        ctx.lineWidth = 2;
+        ctx.ellipse(orbit.planet.x, orbit.planet.y, orbit.distance, orbit.distance/2, 0, 0, cr);
+        ctx.stroke();
+        ctx.setLineDash([]);
+    }
+}
+
+// Render trail
+function renderTrail(body) {
+    var orbit = body.orbit;
+    if (orbit) {
+        ctx.beginPath();
+        ctx.shadowBlur = 10;
+        ctx.shadowColor = body.color;
+        ctx.lineWidth = 4;
         ctx.strokeStyle = body.color + "55";
-        ctx.ellipse(body.orbit.planet.x, body.orbit.planet.y, body.orbit.distance, body.orbit.distance/2, 0, 0, 360);
-        ctx.closePath();
+        ctx.ellipse(orbit.planet.x, orbit.planet.y, orbit.distance, orbit.distance/2, 0, orbit.angle-orbit.speed*200, orbit.angle, orbit.speed<0);
         ctx.stroke();
     }
 }
 
 // Render body (star, planet, death star)
-function renderBody(orb) {
+function renderBody(body) {
     ctx.beginPath();
-    ctx.fillStyle = orb.color;
-    ctx.arc(orb.x, orb.y, orb.size, 0, cr);
-    ctx.closePath();
+    ctx.shadowBlur = body.size;
+    ctx.shadowColor = body.color;
+    ctx.fillStyle = body.color;
+    ctx.arc(body.x, body.y, body.size, 0, cr);
     ctx.fill();
+
+    if (body.orbit) {
+        ctx.beginPath();
+        ctx.shadowBlur = body.size;
+        ctx.shadowColor = "black";
+        ctx.fillStyle = "#000000AA";
+        ctx.arc(body.x, body.y, body.size, body.orbit.angle-cr/4, body.orbit.angle+cr/2-cr/4);
+        ctx.fill();
+    }
 }
