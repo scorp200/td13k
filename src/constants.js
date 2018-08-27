21567daf4406a2b48b897f4d7b683ca7fb817bee
JackOatley <jack.f.oatley@gmail.com>
2018-08-27 15:31:46 +0100

    Renabled constants for size test.

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 var Canvas = document.getElementById("c");
 var ctx = Canvas.getContext("2d", { alpha: false });
-var ENABLE_MUSIC = false;
-var ENABLE_VOICE = false;
+var ENABLE_MUSIC = true;
+var ENABLE_VOICE = true;
 var PI = Math.PI;
 var TAU = PI * 2;
